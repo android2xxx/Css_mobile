9b30dc5373df8f0962b4e2a1b43f853e2a7d4541
antuong12c2 <antuong12c2@gmail.com>
2015-04-13 16:33:36 +0700

    update all

--- app/src/main/java/com/microtecweb/css_mobile/MainMenuActivity.java
+++ app/src/main/java/com/microtecweb/css_mobile/MainMenuActivity.java
@@ -122,6 +122,9 @@ public class MainMenuActivity extends ActionBarActivity {
         switch (position) {
             case 0:
                 fragment = new RequestService();
+                fragmentTransaction.replace(R.id.content_frame, fragment, "RequestServiceFragment");
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
                 break;
             case 1:
                 fragment = new OpenServiceFragment();
@@ -136,6 +139,15 @@ public class MainMenuActivity extends ActionBarActivity {
                 fragmentTransaction.replace(R.id.content_frame, fragment, "ServiceHistoryFragment");
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
+                break;
+            case 5:
+                /*fragment = new ServiceHistoryFragment();
+                fragmentTransaction.replace(R.id.content_frame, fragment, "ChatToLeader");
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                */
+                Intent myIntent = new Intent(this, ChatActivity.class);
+                startActivity(myIntent);
                 break;
             default:
                 break;
@@ -168,6 +180,14 @@ public class MainMenuActivity extends ActionBarActivity {
                 {
                     if(!serviceHistoryFragment.isVisible())
                         fragmentTransaction.show(serviceHistoryFragment);
+                    else
+                        super.onBackPressed();
+                }
+                Fragment requestServiceFragment = getFragmentManager().findFragmentByTag("RequestServiceFragment");
+                if (requestServiceFragment != null)
+                {
+                    if(!requestServiceFragment.isVisible())
+                        fragmentTransaction.show(requestServiceFragment);
                     else
                         super.onBackPressed();
                 }
