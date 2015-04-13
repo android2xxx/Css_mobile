4faf7fc495141e153487bfeff575afc1afe52d84
tupamic <anhtupham@gmail.com>
2015-04-13 16:19:28 +0700

    add ecustomer entity

--- app/src/main/java/com/microtecweb/css_mobile/RequestService.java
+++ app/src/main/java/com/microtecweb/css_mobile/RequestService.java
@@ -1,17 +1,24 @@
 package com.microtecweb.css_mobile;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.List;
-import java.util.ArrayList;
-
-import android.widget.Spinner;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.EConstant;
+import entity.ECustomer;
+import taskserver.QueryHttpGetServiceTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,22 +35,24 @@ public class RequestService extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_open_service_detail, container, false);
-        //Spinner spinnerBank = (Spinner)view.findViewById(R.id.spinnerBank);
-        List<String> list = new ArrayList<String>();
-        list.add("BIDV");
-        list.add("VCB");
-        list.add("VietinBank");
-
-
-        String[] array = new String[list.size()];
-        list.toArray(array);
-
+        View view = inflater.inflate(R.layout.fragment_request, container, false);
         Spinner spinnerBank = (Spinner) view.findViewById(R.id.spinnerBank);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, array) ;
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerBank.setAdapter(adapter);
+        QueryHttpGetServiceTask task = new QueryHttpGetServiceTask(this.getActivity());
+        task.execute(EConstant.URL + "GetCustomer");
+        Gson gson = new Gson();
+
+        try {
+            String json = task.get();
+            Type collectionType = new TypeToken<List<ECustomer>>() {            }.getType();
+            List<ECustomer> lstCustomer = gson.fromJson(json, collectionType);
+            ArrayAdapter<ECustomer> adapter = new ArrayAdapter<ECustomer>(this.getActivity(), android.R.layout.simple_spinner_item, lstCustomer);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            spinnerBank.setAdapter(adapter);
+        } catch (Exception ex) {
+        }
+
         return view;
     }
 
